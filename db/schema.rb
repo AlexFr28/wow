03f04bb6f928d96118251596396b7d0e4f7852f1
Alexandre Cuillerier <alexandre.cuillerier@gmail.com>
2016-06-10 16:55:51 +0200

    add jobs (primary and secondary)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160610092758) do
+ActiveRecord::Schema.define(version: 20160610144629) do
 
   create_table "classes", force: :cascade do |t|
     t.string   "name"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20160610092758) do
 
   create_table "factions", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.string   "name"
+    t.string   "order"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
